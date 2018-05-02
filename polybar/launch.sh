a05c50815ad6b15e354f3918b88200a027ee843e
Simon Sawert <simon@sawert.se>
2018-05-02 16:59:47 +0200

    Adaptions for i3 and polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-for m in $(polybar -m primary | awk -F':' '{print $1}'); do
+for m in $(polybar -m primary | awk -F':' '{print $1}' | sort -r); do
     MONITOR=$m polybar default &
 done
 
