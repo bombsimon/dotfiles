15eaec5aa559ad1ec3c92ceed59f74254a4641dd
Simon Sawert <simon@sawert.se>
2021-11-29 14:50:34 +0100

    Improve VS Code config

--- vscode/install-packages.sh
+++ vscode/install-packages.sh
@@ -5,25 +5,26 @@ packages="
   bazelbuild.vscode-bazel
   bierner.markdown-preview-github-styles
   cheshirekow.cmake-format
+  cpptools-linux.vsix
   davidanson.vscode-markdownlint
   dbaeumer.vscode-eslint
   eamodio.gitlens
   elixir-lsp.elixir-ls
   elmtooling.elm-ls-vscode
+  equinusocio.vsc-material-theme
   esbenp.prettier-vscode
   golang.go
   haskell.haskell
   heptio.jsonnet
   hoovercj.haskell-linter
-  jdinhlife.gruvbox
   mads-hartmann.bash-ide-vscode
   mblode.pretty-formatter
   ms-python.python
   ms-vscode.cmake-tools
+  pkief.material-icon-theme
   redhat.java
   redhat.vscode-yaml
   rust-lang.rust
-  sainnhe.gruvbox-material
   sfodje.perlcritic
   sfodje.perltidy
   shardulm94.trailing-spaces
