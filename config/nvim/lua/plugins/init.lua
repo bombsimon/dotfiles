7fe6ebdde68eb193725c339fabfce93298172906
Simon Sawert <simon@sawert.se>
2025-02-05 21:26:53 +0100

    Add VenvSelect

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -34,6 +34,20 @@ return {
         opts = {}
     },
     {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "nvim-telescope/telescope.nvim",
+            "mfussenegger/nvim-dap-python",
+        },
+        opts = {},
+        branch = "regexp",
+        keys = {
+            { "<leader>vs", "<cmd>VenvSelect<cr>" },
+            { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+        },
+    },
+    {
         "akinsho/bufferline.nvim",
         version = "*",
         dependencies = "nvim-tree/nvim-web-devicons",
