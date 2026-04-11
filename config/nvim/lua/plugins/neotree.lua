a58b6575589ea45444cf876e621344e5415c7421
Simon Sawert <simon@sawert.se>
2026-04-11 16:19:13 +0200

    Improve slowness on write

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -18,8 +18,20 @@ return {
 
     require("neo-tree").setup({
       close_if_last_window = true,
+      -- The git_status source is very slow to refresh on write (#1284).
+      -- Disable global refresh-on-write and only re-subscribe filesystem.
+      -- Use `R` to manually refresh git_status when needed.
+      enable_refresh_on_write = false,
       position = "left",
       width = 40,
+      event_handlers = {
+        {
+          event = "vim_buffer_changed",
+          handler = function()
+            require("neo-tree.sources.manager").refresh("filesystem")
+          end,
+        },
+      },
       window = {
         mappings = {
           ["<cr>"] = "open_with_window_picker",
@@ -29,7 +41,7 @@ return {
         filtered_items = {
           hide_dotfiles = false,
           hide_hidden = false,
-          hide_gitignored = false,
+          hide_gitignored = true,
         },
         -- toggle this feature
         -- https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1485#issuecomment-2164865976
