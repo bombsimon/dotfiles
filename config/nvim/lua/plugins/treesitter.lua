46349cceeea8a8c4607e8a76daa824f68f12ea5c
Simon Sawert <simon@sawert.se>
2026-07-27 23:01:48 +0200

    Add Monkey C tree sitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,10 @@ return {
   version = false,
   build = ":TSUpdate",
   config = function()
+    -- `.mc` -> monkeyc filetype detection comes from garmin-monkeyc.nvim
+    -- Installed via https://github.com/bombsimon/tree-sitter-monkey-c
+    vim.treesitter.language.register("monkey_c", { "monkeyc" })
+
     require("nvim-treesitter").install({
       "gleam",
       "go",
